0b30d9ab678bb68bb245bc4c2af81b862bad3de2
Morten Munk <mortenmunk97@gmail.com>
2025-10-12 14:35:17 +0200

    flake simplified

--- flake.nix
+++ flake.nix
@@ -1,66 +1,31 @@
 {
+  description = "Development environment for haskell.";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    haskell-flake.url = "github:srid/haskell-flake";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = inputs @ {
-    self,
+
+  outputs = {
     nixpkgs,
-    flake-parts,
+    flake-utils,
     ...
   }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = nixpkgs.lib.systems.flakeExposed;
-      imports = [inputs.haskell-flake.flakeModule];
-
-      perSystem = {
-        self',
-        pkgs,
-        ...
-      }: {
-        # Typically, you just want a single project named "default". But
-        # multiple projects are also possible, each using different GHC version.
-        haskellProjects.default = {
-          # The base package set representing a specific GHC version.
-          # By default, this is pkgs.haskellPackages.
-          # You may also create your own. See https://community.flake.parts/haskell-flake/package-set
-          # basePackages = pkgs.haskellPackages;
-
-          # Extra package information. See https://community.flake.parts/haskell-flake/dependency
-          #
-          # Note that local packages are automatically included in `packages`
-          # (defined by `defaults.packages` option).
-          #
-          packages = {
-            # aeson.source = "1.5.0.0";      # Override aeson to a custom version from Hackage
-            # shower.source = inputs.shower; # Override shower to a custom source path
-          };
-          settings = {
-            #  aeson = {
-            #    check = false;
-            #  };
-            #  relude = {
-            #    haddock = false;
-            #    broken = false;
-            #  };
-          };
-
-          devShell = {
-            # Enabled by default
-            # enable = true;
-
-            # Programs you want to make available in the shell.
-            # Default programs can be disabled by setting to 'null'
-            # tools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
-
-            # Check that haskell-language-server works
-            # hlsCheck.enable = true; # Requires sandbox to be disabled
-          };
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
         };
-
-        # haskell-flake doesn't set the default package, but you can do it here.
-        packages.default = self'.packages.haskell-fives;
-      };
-    };
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              ghc
+              haskell-language-server
+              ormolu
+              cabal-install
+            ];
+          };
+      }
+    );
 }
