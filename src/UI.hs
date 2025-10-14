5e47eeeef27547e722cfee3848972190813add87
Morten Munk <mortenmunk97@gmail.com>
2025-10-14 22:35:11 +0200

    Tile design

--- src/UI.hs
+++ src/UI.hs
@@ -3,7 +3,7 @@ module UI (runUI) where
 import Board
 import Boneyard
 import Brick
-import Brick.Widgets.Border (border, borderWithLabel)
+import Brick.Widgets.Border (border, borderWithLabel, hBorder, vBorder)
 import Brick.Widgets.Center (center, hCenter)
 import qualified Graphics.Vty as V
 import Player
@@ -12,7 +12,7 @@ import Tile
 data St = St {player :: Player, enemy :: Player, board :: Board, boneyard :: Boneyard Tile}
 
 drawUI :: St -> [Widget ()]
-drawUI _ =
+drawUI s =
   [ padAll
       1
       ( borderWithLabel
@@ -22,7 +22,11 @@ drawUI _ =
                   [ fill ' ',
                     legendBox
                   ],
-                center (str "Game goes here...")
+                padTop
+                  (Pad 1)
+                  (center (str "Game goes here...")),
+                padTop (Pad 1) hBorder,
+                padTop (Pad 1) (hCenter (renderHand (hand (player s))))
               ]
           )
       )
@@ -46,6 +50,70 @@ legendBox =
             )
         )
     )
+
+renderPips :: Int -> Widget ()
+renderPips n =
+  let dot b = if b then "●" else " "
+      row a b c = hCenter (hBox [str (dot a), str (dot b), str (dot c)])
+   in vBox
+        ( case n of
+            0 ->
+              [ row False False False,
+                row False False False,
+                row False False False
+              ]
+            1 ->
+              [ row False False False,
+                row False True False,
+                row False False False
+              ]
+            2 ->
+              [ row True False False,
+                row False False False,
+                row False False True
+              ]
+            3 ->
+              [ row True False False,
+                row False True False,
+                row False False True
+              ]
+            4 ->
+              [ row True False True,
+                row False False False,
+                row True False True
+              ]
+            5 ->
+              [ row True False True,
+                row False True False,
+                row True False True
+              ]
+            6 ->
+              [ row True False True,
+                row True False True,
+                row True False True
+              ]
+            _ ->
+              [ row False False False,
+                row False False False,
+                row False False False
+              ]
+        )
+
+renderTile :: Tile -> Widget ()
+renderTile (Tile l r) =
+  vLimit 5 $
+    hLimit 13 $
+      border
+        ( hBox
+            [ padLeftRight 1 (renderPips (fromEnum l)),
+              vBorder,
+              padLeftRight 1 (renderPips (fromEnum r))
+            ]
+        )
+
+renderHand :: Hand Tile -> Widget ()
+renderHand (Hand tiles) =
+  hBox (map (padRight (Pad 1) . renderTile) tiles)
 
 handleEvent :: BrickEvent () e -> EventM n s ()
 handleEvent (VtyEvent (V.EvKey (V.KChar 'q') [])) = halt
